41d38a7dd9879a2d10966672fd18b0ae23dc0f4d
Thanachit <scophia.1@gmail.com>
2025-09-04 07:03:23 +0700

    Update sports.js

--- api/sports.js
+++ api/sports.js
@@ -1,19 +1,13 @@
 // api/sports.js
-
 export default async function handler(req, res) {
   try {
     const response = await fetch(
-      "https://api.the-odds-api.com/v4/sports",
-      {
-        headers: {
-          "x-api-key": "eb3db6ad48c8d563a220c9a155856991", // 👈 วาง API KEY จริงตรงนี้
-        },
-      }
+      `https://api.the-odds-api.com/v4/sports/?apiKey=eb3db6ad48c8d563a220c9a155856991`
     );
 
     const data = await response.json();
     res.status(response.status).json(data);
   } catch (error) {
-    res.status(500).json({ error: "Proxy error", details: error.message });
+    res.status(500).json({ error: "Proxy request failed", details: error.message });
   }
 }
